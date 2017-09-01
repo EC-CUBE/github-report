d98c4940574e41a3f7d7c9e4ce4a967d0d2f7a65
kiy0taka <kiy0taka333@gmail.com>
2017-09-01 11:07:19 +0900

    init.

---
+++ app.js
@@ -0,0 +1,22 @@
+const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN;
+const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
+
+const GitHub = require('github-api');
+const SlackClient = require('@slack/client').WebClient;
+const gh = new GitHub();
+
+function postToSlack(message, opts) {
+    if (SLACK_API_TOKEN) {
+        let web = new SlackClient(SLACK_API_TOKEN);
+        web.chat.postMessage(SLACK_CHANNEL, message, opts);
+    }
+}
+
+gh.getIssues('EC-CUBE', 'ec-cube').listIssues({"milestone":"none", "state":"open"}).then(function(data) {
+    if (data.data) {
+        let issueUrls = data.data.map(function(issue) {
+            return issue.html_url
+        });
+        postToSlack(issueUrls.join('\n'), {username:'本日のNo Milestone'});
+    }
+});
