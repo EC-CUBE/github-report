e7ecfd9e8af8ffacbdea1bdd88d4702feb3148ca
kiy0taka <kiy0taka333@gmail.com>
2017-09-06 17:55:14 +0900

    エラー処理を改善

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
 const GitHub = require('github-api');
 const SlackClient = require('@slack/client').WebClient;
 const gh = new GitHub({'token': GITHUB_TOKEN});
+const co = require('co');
 
-function postToSlack(message, opts) {
-    if (SLACK_API_TOKEN) {
-        let web = new SlackClient(SLACK_API_TOKEN);
-        web.chat.postMessage(SLACK_CHANNEL, message, opts);
-    }
-}
-
-gh.getIssues('EC-CUBE', 'ec-cube').listIssues({"milestone":"none", "state":"open"}).then(data => {
+co(function*() {
+    let data = yield gh.getIssues('EC-CUBE', 'ec-cube').listIssues({"milestone":"none", "state":"open"});
     if (data.data.length) {
         let issueUrls = data.data.map(issue => issue.html_url);
-        postToSlack(issueUrls.join('\n'), {username:'本日のNo Milestone'});
+
+        if (SLACK_API_TOKEN) {
+            let web = new SlackClient(SLACK_API_TOKEN);
+            yield web.chat.postMessage(SLACK_CHANNEL, issueUrls.join('\n'), {username:'本日のNo Milestone'});
+        }
     }
 }).catch(e => {
     console.log(e);
